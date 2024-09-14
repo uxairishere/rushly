4224d45db37840dfbd154929b218cc30450be921
Uxair Abbas <uzaaaaaair@gmail.com>
2024-09-15 00:42:21 +0500

    feat(structure): Implemented app structure

---
+++ app/(root)/(tabs)/home.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello these</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
